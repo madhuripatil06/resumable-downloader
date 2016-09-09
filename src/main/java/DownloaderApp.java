470db9a33ff4479a2174f6d22205bc7c3b5ac7a4
Pankaj Sharma <sharmapankaj2512@gmail.com>
2016-09-09 15:02:01 +0530

    Show message if file is already downloaded

--- src/main/java/DownloaderApp.java
+++ src/main/java/DownloaderApp.java
@@ -11,12 +11,16 @@ public class DownloaderApp {
     private static final String SOURCE_PATH = "http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_2016-05-17.zip";
     private static final String DESTINATION_PATH = "src/main/resources";
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        RemoteFile remoteFile = new RemoteFile(SOURCE_PATH, DESTINATION_PATH);
-        OutputStream outputStream = new OutputStream(remoteFile, 1024);
-        DownloadManager manager = new DownloadManager(outputStream);
-        manager.start();
-        displayProgress(manager);
+    public static void main(String[] args) throws InterruptedException {
+        try {
+            RemoteFile remoteFile = new RemoteFile(SOURCE_PATH, DESTINATION_PATH);
+            OutputStream outputStream = new OutputStream(remoteFile, 1024);
+            DownloadManager manager = new DownloadManager(outputStream);
+            manager.start();
+            displayProgress(manager);
+        } catch (IOException ex) {
+            System.out.println("Download complete.");
+        }
     }
 
     private static void displayProgress(DownloadManager manager) throws InterruptedException {
