4d312251d55c4d95285d0931d694ad3f162f2362
Pankaj Sharma <sharmapankaj2512@gmail.com>
2016-09-09 14:42:02 +0530

    show progressing slash

--- src/main/java/DownloaderApp.java
+++ src/main/java/DownloaderApp.java
@@ -11,12 +11,22 @@ public class DownloaderApp {
     private static final String SOURCE_PATH = "http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_2016-05-17.zip";
     private static final String DESTINATION_PATH = "src/main/resources";
 
-    public static void main() throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         RemoteFile remoteFile = new RemoteFile(SOURCE_PATH, DESTINATION_PATH);
         OutputStream outputStream = new OutputStream(remoteFile, 1024);
         DownloadManager manager = new DownloadManager(outputStream);
         manager.start();
+        displayProgress(manager);
+    }
+
+    private static void displayProgress(DownloadManager manager) throws InterruptedException {
         while (!manager.isDownloadComplete())
-            System.out.print(".");
+        {
+            for (String phase : new String[]{"|", "/", "-", "\\"})
+            {
+                System.out.printf("\r\b Downloading... " + phase);
+                Thread.sleep(100);
+            }
+        }
     }
 }
