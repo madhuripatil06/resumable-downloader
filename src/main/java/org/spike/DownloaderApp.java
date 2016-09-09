a484d267bd8a293982393a34021f3fbd384ab3ff
Pankaj Sharma <sharmapankaj2512@gmail.com>
2016-09-09 20:44:12 +0530

    move download manager to its own initializer

--- src/main/java/org/spike/DownloaderApp.java
+++ src/main/java/org/spike/DownloaderApp.java
@@ -1,5 +1,6 @@
 package org.spike;
 
+import com.google.common.base.Preconditions;
 import org.spike.domain.RemoteFile;
 import org.spike.downloader.DownloadManager;
 import org.spike.io.OutputStream;
@@ -11,15 +12,25 @@ import java.io.IOException;
  * Created by pankajs on 09/09/16.
  */
 public class DownloaderApp {
+    private static  DownloadManager manager;
+
     public static void main(String[] args) throws InterruptedException {
         try {
-            RemoteFile remoteFile = new RemoteFile(args[0], args[1]);
-            OutputStream outputStream = new OutputStream(remoteFile, 1024);
-            DownloadManager downloadManager = new DownloadManager(outputStream);
+            Preconditions.checkArgument(args.length == 2, "url & location are mandatory parameters");
+            
+            DownloadManager downloadManager = initialize(args);
             downloadManager.start();
             new ProgressBar(downloadManager).display("Downloading...");
         } catch (IOException ex) {
             System.out.println("Download complete.");
         }
+    }
+
+    private static DownloadManager initialize(String[] args) throws IOException {
+        String url = args[0];
+        String location = args[1];
+        RemoteFile remoteFile = new RemoteFile(url, location);
+        OutputStream outputStream = new OutputStream(remoteFile, 1024);
+        return new DownloadManager(outputStream);
     }
 }
