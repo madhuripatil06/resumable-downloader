631e08737a6ae5f1c8e03559f1fa48528a8acf9e
Pankaj Sharma <sharmapankaj2512@gmail.com>
2016-09-09 18:37:06 +0530

    replace loop with stream

--- src/main/java/org/spike/downloader/ProgressBar.java
+++ src/main/java/org/spike/downloader/ProgressBar.java
@@ -1,5 +1,7 @@
 package org.spike.downloader;
 
+import java.util.Arrays;
+
 /**
  * Created by pankajs on 09/09/16.
  */
@@ -13,11 +15,17 @@ public class ProgressBar {
     public void display(String message) throws InterruptedException {
         while (trackable.isRunning())
         {
-            for (String phase : new String[]{"|", "/", "-", "\\"})
-            {
-                System.out.printf("\r\b " + message + " " + phase);
-                Thread.sleep(100);
-            }
+            String[] shapes = new String[]{"|", "/", "-", "\\"};
+            Arrays.stream(shapes).forEach(shape -> draw(message, shape));
+        }
+    }
+
+    private void draw(String message, String shape) {
+        try {
+            System.out.printf("\r\b " + message + " " + shape);
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
