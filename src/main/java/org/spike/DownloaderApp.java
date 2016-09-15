193025d53233490e4905c0c350660c61f4538943
madhuripatil06 <madhuri@thoughtworks.com>
2016-09-15 14:41:40 +0530

    add feature support for multiple file download

--- src/main/java/org/spike/DownloaderApp.java
+++ src/main/java/org/spike/DownloaderApp.java
@@ -3,31 +3,41 @@ package org.spike;
 import com.google.common.base.Preconditions;
 import org.spike.domain.RemoteFile;
 import org.spike.downloader.DownloadManager;
-import org.spike.io.Worker;
-import org.spike.downloader.ProgressBar;
-import org.spike.net.HttpRangeConnection;
 
 import java.io.IOException;
-import java.util.Timer;
 
 
 public class DownloaderApp {
     public static void main(String[] args) throws InterruptedException {
         try {
-            Preconditions.checkArgument(args.length == 2, "url & location are mandatory parameters");
-            RemoteFile remoteFile= new RemoteFile(url(args), location(args));
-            DownloadManager downloadManager = new DownloadManager(remoteFile);
-            downloadManager.start();
+            Preconditions.checkArgument(args.length >= 2, "url & location are mandatory parameters");
+            processs(args);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    private static String location(String[] args) {
-        return args[1];
+    private static void processs(String[] args) throws IOException {
+        for (String url : urls(args)) {
+            downloadFile(url, location(args));
+        }
     }
 
-    private static String url(String[] args) {
-        return args[0];
+    private static void downloadFile(String url, String location) throws IOException {
+        RemoteFile remoteFile= new RemoteFile(url, location);
+        DownloadManager downloadManager = new DownloadManager(remoteFile);
+        downloadManager.start();
+    }
+
+
+    private static String location(String[] args) {
+        return args[args.length-1];
+    }
+
+    private static String[] urls(String[] args) {
+        String [] allUrls = new String[args.length-1];
+        for(int i = 0 ; i < args.length-1 ; i++)
+            allUrls[i] = args[i];
+        return allUrls;
     }
 }
